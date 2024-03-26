3a112bc832aa54a50a1ab6d66d1047a8fe9e3aa4
saumanarsad <sauman.arshad@gmail.com>
2024-03-27 02:05:27 +0500

    Used functions and conditions

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,10 +1,23 @@
-void main() {
-  var sauman = Humans();
-  print(sauman.password);
-}
+// void main() {
+//   var sauman = Humans();
+//   var name = sauman.password;
+//   print("My name is $name");
+// }
 
-class Humans {
-  var email = '@ sauman';
-  var password = 'password';
-  
+// class Humans {
+//   var email = '@ sauman';
+//   var password = 'password';
+// }
+import 'dart:ffi';
+
+void main() {
+  String say(String from, String message, [String? device]) {
+    var result = "$from says $message ";
+    if (device != null) {
+      result = "$from with a $device says $message";
+    }
+    return result;
+  }
+
+  assert(say('Sauman', 'hello') == 'Sauman says hello');
 }
